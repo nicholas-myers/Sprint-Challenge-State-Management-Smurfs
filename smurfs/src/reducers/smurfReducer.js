09f93a8fc95d841288a3bff35d94e9ec387bddfd
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-15 08:42:42 -0700

    installed redux dependencies and setup smurfReducer and create store with redux thunk and logger passed through middleware

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    loadingSmurfs: false,
+    smurfs: [],
+    smurfingError: ""
+}
+
+export const smurfReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
